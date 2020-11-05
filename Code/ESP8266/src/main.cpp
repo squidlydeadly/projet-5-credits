96ddcfb74a01e4f9c5bf0ec3aca233ace0096b08
squidlydeadly <38928742+squidlydeadly@users.noreply.github.com>
2020-11-05 13:49:28 -0500

    Added preuve de concepte

---
+++ Code/ESP8266/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+//int LED_BUILTIN = 2;
+
+void setup() {
+pinMode (2, OUTPUT);
+}
+void loop() {
+digitalWrite(2, HIGH);
+delay(1000);
+digitalWrite(2, LOW);
+delay(1000);
+}
